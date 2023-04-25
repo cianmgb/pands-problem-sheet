ac2d775218aec0dbac79742fda1c0a1816057004
Cian Mac Giolla Bhride <cian.mgb@gmail.com>
2023-04-25 20:19:41 +0100

    poor attempt at getting the code to work

---
+++ openfilename.py
@@ -0,0 +1,6 @@
+#Pull a sample text that has letters in it - I am using README as it is in the file
+filename = README.md
+#open the file
+with open(filename,'r')
+# using count()
+return text.count('e')
