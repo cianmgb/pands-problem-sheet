b591dce3ec44d93bdfa17307152636d65dd7039f
cianmgb <124096586+cianmgb@users.noreply.github.com>
2023-04-25 20:22:40 +0100

    Delete accounts.py

--- accounts.py
+++
@@ -1,6 +0,0 @@
-#Ask for the bank account number
-Accountstring = input("Please enter your account number ")
-#Get the last 4 digits
-AccountShowingonlylast4digits = Accountstring[-4:].rjust(len(Accountstring), 'X')
-#Print the last four digits only, with X replacing all recurring digits
-print(AccountShowingonlylast4digits)
